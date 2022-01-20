b1fb9b9a1612586aa63a447d4f76913d1caf459a
Rafael Kuvasney Marcolin (MIOJO) <kuvasney@gmail.com>
2022-01-19 22:46:33 -0300

    criacao do projeto e assets da home

---
+++ layouts/default.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="pagetemplate">
+    <!-- <Header v-if="this.$route.fullPath != '/'" />
+    <Sidebar />
+    <loading-messages :active="isLoading" ref="loader"></loading-messages> -->
+    <nuxt />
+    <!-- <Footer /> -->
+  </div>
+</template>
+<script>
+export default {
+  name: 'DefaultLayout'
+}
+</script>
+<style lang="scss">
+  html {
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  body {
+    font-weight: 400;
+    font-size: 1em;
+    word-spacing: 1px;
+    padding: 0;
+    margin: 0;
+  }
+
+  .pagetemplate {
+    min-width: 100vw;
+    min-height: 100vh;
+    padding: 0;
+    margin: 0;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template src="../partials/home/home.html" />
+<script src="../partials/home/home.js" /></script>
+<style lang="scss" src="../partials/home/home.scss" scoped></style>
