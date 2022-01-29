3cc07673d7e9288ec1f8ea61ca5becf3d53f8e10
Rafael Kuvasney Marcolin (MIOJO) <kuvasney@gmail.com>
2022-01-29 11:17:27 -0300

    speedometer done

--- partials/metodoPagamento/metodo-pagamento.js
+++ partials/metodoPagamento/metodo-pagamento.js
@@ -2,12 +2,10 @@ export default {
   name: 'metodo-de-pagamento',
   data () {
     return {
-
+      debtValue: 4832.66,
       paymentGadget: {
         minValue: 1,
-        maxValue: 100,
-        debtValue: 3180.5,
-        parcel: 10,
+        parcel: Number,
         value: 50
       },
       gadgetValue: 50,
@@ -18,18 +16,42 @@ export default {
     }
   },
   watch: {
-    gadgetValue () {
-      this.parcelValue(this.gadgetValue)
-    }
   },
   methods: {
-    parcelValue (v) {
-      const _tv = this.paymentGadget.debtValue
-      const _parcel = (_tv / 100) * v
-      this.paymentGadget.parcel = _parcel.toFixed(2)
-      // this.speedometer.pointer = parseInt(v) >= 50 ? parseInt(v) * 2.2 - 110 : parseInt(v) * 2.2 - 110
-      this.speedometer.pointer = parseInt(v) * 2 - 100
-      this.speedometer.groove = parseInt(v) * 1.8 + 315
+    /**
+     * Starting methods for metodo-de-pagamento
+     */
+    init () {
+      this.paymentGadget.parcel = (this.debtValue / 2).toFixed(2)
+      this.setSpeedometer(50)
+    },
+    /**
+     * Set the parcel value
+     * @param {number} pValue parcel value wished
+     * @param {number} pRange range
+     * @param {number} mode the modes: 1 moves the range, 2 changes the money input
+     */
+    parcelValue (pValue, pRange, mode) {
+      let _ret
+      if (mode === 2) {
+        _ret = pRange
+        this.paymentGadget.parcel = ((pValue / 100) * pRange).toFixed(2)
+      } else {
+        _ret = (pValue / this.debtValue) * 100
+        this.gadgetValue = _ret
+      }
+      this.setSpeedometer(_ret)
+    },
+    /**
+     * Moves the speedometer
+     * @param {num} spoRange range
+     */
+    setSpeedometer (spoRange) {
+      this.speedometer.pointer = spoRange * 2.2 - 110
+      this.speedometer.groove = parseInt(spoRange) * 1.8 + 315
     }
+  },
+  mounted () {
+    this.init()
   }
 }
