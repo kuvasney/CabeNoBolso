85946c1df4ae9a4c31bfc63c9bb3909da951413d
Rafael Kuvasney Marcolin (MIOJO) <kuvasney@gmail.com>
2022-02-08 00:00:56 -0300

    ofertas

---
+++ pages/nossas-ofertas.vue
@@ -0,0 +1,3 @@
+<template src="../partials/nossasOfertas/ofertas.html" />
+<script src="../partials/nossasOfertas/ofertas.js" /></script>
+<style lang="scss" src="../partials/nossasOfertas/ofertas.scss" scoped></style>
